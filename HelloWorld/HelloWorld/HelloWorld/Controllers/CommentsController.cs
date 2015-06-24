0718e5b87f7c131a0e67491864abd24219c5418f
ivalchenko <i.valchenk@hotmail.com>
2015-06-24 09:54:20 +0300

    Added stable version

---
+++ HelloWorld/HelloWorld/HelloWorld/Controllers/CommentsController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using HelloWorld.Models;
+using Microsoft.AspNet.Identity;
+
+namespace HelloWorld.Controllers
+{
+    public class CommentsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // POST: Comments/Create
+        [HttpPost]
+        //[ValidateAntiForgeryToken]
+        public ActionResult Create(Comment comment, int? PostId)
+        {
+            if (ModelState.IsValid)
+            {
+                //comment.User = db.Users.Find(User.Identity.GetUserId());
+                //comment.User = null;
+                comment.AuthorId = User.Identity.GetUserId();
+
+                Post post = db.Posts.Find(PostId);
+                comment.CommentDate = DateTime.Now;
+                comment.Post = post;
+                db.Posts.Find(comment.Post.PostId).CommentsNumber += 1;
+                db.Comments.Add(comment);
+                db.SaveChanges();
+                return RedirectToAction("Details", "Posts", new { id = comment.Post.PostId });
+            }
+
+            return View(comment);
+        }
+
+        // POST: Comments/Delete/5
+        [HttpPost, ActionName("Delete")]
+        //[ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Comment comment = db.Comments.Find(id);
+            var postId = db.Comments.Include(s => s.Post).FirstOrDefault(s => s.CommentId == id).Post.PostId;
+            db.Comments.Remove(comment);
+            db.Posts.Find(postId).CommentsNumber -= 1;
+            db.SaveChanges();
+            return RedirectToAction("Details", "Posts", new { id = postId });
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ HelloWorld/HelloWorld/HelloWorld/Models/PostComment.cs
@@ -0,0 +1,23 @@
+﻿using System.Data;
+using System.Data.Entity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+
+namespace HelloWorld.Models
+{
+    /*public class PostComment
+    {
+        [Required]
+        public Post Post { get; set; }
+
+        [Required]
+        public List<Comment> Comments { get; set; }
+
+        [Required]
+        public int PostCommentId { get; set; }
+    }*/
+}
